458f19b35c8160516eed1dadf3bc1f5750e93d23
DzmitryMiankou <gmiankou@gmail.com>
2024-03-14 10:26:56 +0300

    dfgnf

--- js/script.js
+++ js/script.js
@@ -5,28 +5,16 @@ window.addEventListener("load", function () {
 });
 
 const mainWrapper = document.querySelector(".main__wrapper");
-
-/*
-const sections = [...document.querySelectorAll("[scroll-el]")];
-const text = [...document.querySelectorAll(".header-nav__link")];
-const logoText = document.querySelector(".logo__text");
-const header = document.querySelector(".header");
-const rectLogo = document.querySelector(".logo__rect");
-const homeArt = document.querySelector(".home-art");
-const main = document.querySelector(".main");
-const footer = document.querySelector(".footer");
-const aside = document.querySelector(".aside");
-const asideItems = document.querySelector(".aside-items");
-const artItem = [...document.querySelectorAll(".artists-items__item")];
 const HEADER_NAV = document.querySelector("." + "header-nav");
 const HEADER_BLUER = document.querySelector(".header__bluer");
 const whatDevice = /iPad|iPhone|iPod/.test(navigator.userAgent);
 const whatEventUse = whatDevice ? "touchstart" : "click";
 const SLIDER = document.querySelector("." + "reviews-slider__content");
-
+const buttonUp = document.querySelector("#up");
 const BUTTONS_SLIDERL = document.querySelector("#left");
 const BUTTONS_SLIDERR = document.querySelector("#right");
 
+const formWrapper = document.querySelector(".form__wrapper");
 let openW = false;
 function openHeaderMenu(event) {
   if (event.target.closest("." + "header__button")) {
@@ -50,65 +38,6 @@ function openHeaderMenu(event) {
     return () => clearTimeout(timer);
   }
 }
-
-function observeControl() {
-  if (aside) {
-    aside.style.height = main.offsetHeight - header.offsetHeight + "px";
-    asideItems.style.height =
-      main.offsetHeight - header.offsetHeight - 150 + "px";
-  }
-  if (homeArt) {
-    const scrollRoot = homeArt.offsetHeight + header.offsetHeight;
-    const options = {
-      rootMargin: `${scrollRoot * -1}px 0px ${scrollRoot + 4}px 0px`,
-      threshold: 0,
-    };
-
-    function handleIntersection(entries, options) {
-      entries.forEach((entry) => {
-        if (!entry.isIntersecting) {
-          text.forEach((e) => e.classList.add("header-nav__link--theme"));
-          header.classList.add("header--theme");
-          rectLogo.classList.add("logo__rect--theme");
-          logoText.classList.add("logo__text--thema");
-        }
-        if (entry.isIntersecting) {
-          text.forEach((e) => e.classList.remove("header-nav__link--theme"));
-          header.classList.remove("header--theme");
-          rectLogo.classList.remove("logo__rect--theme");
-          logoText.classList.remove("logo__text--thema");
-          homeArt.classList.remove("home-art--theme");
-        }
-      }, options);
-    }
-
-    const observer = new IntersectionObserver(handleIntersection, options);
-    sections.forEach((section) => {
-      observer.observe(section);
-    });
-  }
-}
-
-function focusController(el, className) {
-  const addCass = (elem) => elem.forEach((st) => st.classList.add(className));
-  const delCass = (elem) =>
-    elem.forEach((st) => st.classList.remove(className));
-
-  el.forEach((e) => {
-    e.addEventListener("mousemove", () => addCass([e.children[0]]));
-  });
-  el.forEach((e) =>
-    e.addEventListener("mouseout", () => delCass([e.children[0]]))
-  );
-}*/
-const whatDevice = /iPad|iPhone|iPod/.test(navigator.userAgent);
-const whatEventUse = whatDevice ? "touchstart" : "click";
-const SLIDER = document.querySelector("." + "reviews-slider__content");
-const buttonUp = document.querySelector("#up");
-const BUTTONS_SLIDERL = document.querySelector("#left");
-const BUTTONS_SLIDERR = document.querySelector("#right");
-
-const formWrapper = document.querySelector(".form__wrapper");
 
 const init = 320;
 function onClickHandler() {
@@ -145,14 +74,7 @@ function start() {
   onClickHandler();
   ubButtonController();
 
-  /* window.addEventListener("resize", () => observeControl());
-  observeControl();
-  focusController(artItem, "artists-items__content--active");
-  focusController([footer], "footer--theme");
-
   window.addEventListener(whatEventUse, (event) => {
     openHeaderMenu(event);
   });
-
-  return () => window.addEventListener("resize", () => observeControl());*/
 }
